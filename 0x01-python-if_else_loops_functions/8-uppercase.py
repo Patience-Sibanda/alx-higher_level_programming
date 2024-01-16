2cf18a339f165731bfbe49e2f09a48078d43e9bb
Patience-Sibanda <psa.khanye@gmail.com>
2024-01-16 00:16:00 -0800

    0-positive_or_negative.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(s):
+    result = ""
+
+    for char in s:
+        if 97 <= ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+
+    print(result)
